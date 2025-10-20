736fe5a3ca2bfe68799b19d32b05f41a5e24fe25
namanhere23 <namanhere23code@gmail.com>
2025-10-21 03:50:43 +0530

    Media Upload Backend

---
+++ Backend/src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler=(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise.resolve(requestHandler(req,res,next))
+        .catch((err)=> next(err))
+    }
+}
+
+export default asyncHandler
+
+// const asyncHandler=(fn)=> async(req,res,next)=>{
+//     try{
+//         await fn(req,res,next)
+//     } catch(error){
+//         res.error(err.code).json({
+//             success: false,
+//             message:err.message
+//         })
+//     }
+// }
